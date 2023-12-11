41802f9574179e45fa15c2ab271231148a7a1870
poloju-rishith <rishith2797@gmail.com>
2023-12-11 21:00:47 +0530

    router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './components/home'
+import Router from './router'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Home />
+		<Router />
 	</React.StrictMode>
 )
