816c9a156b07383e6f732a8e68f3e048c9d1a70e
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2023-12-28 22:22:05 +0500

    update index.tsx file

---
+++ src/components/basket/basket.tsx
@@ -0,0 +1,84 @@
+import { Flex } from 'antd'
+import AddBasketModal from './modals/add-new-basket'
+import SaveBasket from './container/saved-basket-container'
+import ConfirmModal from './modals/confitm-modal'
+import BasketContainer from './container/baskets-container'
+import EditBasketModal from './modals/edit-basket-modal'
+import { useBasketStore } from './store/basket-store'
+
+const Basket = () => {
+	const {
+		isAddBasketModalOpen,
+		duplicateError,
+		isEditModalOpen,
+		setDuplicateError,
+		closeModalConfirmation,
+		closeEditConfirmation,
+		toogleEditModal,
+		timeErrorModalOpen,
+		toggleTimeErrorModalOpen,
+		setEmptyBasketError,
+		emptyBasketError,
+	} = useBasketStore()
+
+	const handleCloseConfirmModal = (val: boolean) => {
+		toogleEditModal(false)
+		closeEditConfirmation(val)
+	}
+	const handleCancelConfirmModal = (val: boolean) => {
+		closeEditConfirmation(val)
+	}
+
+	const handleTimeErrorModalClose = () => {
+		toggleTimeErrorModalOpen(false)
+	}
+
+	const handleEmptyBasketErrorModalClose = () => {
+		setEmptyBasketError(false)
+	}
+
+	return (
+		<>
+			<Flex className="h-[100vh] overflow-y-hidden overflow-x-hidden">
+				<Flex flex={'1'}>
+					<SaveBasket />
+				</Flex>
+				<Flex flex={3}>
+					<BasketContainer />
+				</Flex>
+			</Flex>
+			{isEditModalOpen && <EditBasketModal open={isEditModalOpen} />}
+			{<AddBasketModal open={isAddBasketModalOpen} />}
+			<ConfirmModal
+				open={duplicateError}
+				handleOpen={setDuplicateError}
+				handleCancel={setDuplicateError}
+				header={'Duplicate Alert'}
+				message="Basket with this name already exists"
+			/>
+			<ConfirmModal
+				open={closeModalConfirmation}
+				handleOpen={handleCloseConfirmModal}
+				handleCancel={handleCancelConfirmModal}
+				header={'Alert!'}
+				message="Are you sure you want to close modal? All unsaved data will be lost!"
+			/>
+			<ConfirmModal
+				open={timeErrorModalOpen}
+				handleOpen={handleTimeErrorModalClose}
+				handleCancel={handleTimeErrorModalClose}
+				header={'Time Error!'}
+				message="Exit Time Should be after Entry Time!"
+			/>
+			<ConfirmModal
+				open={emptyBasketError}
+				handleOpen={handleEmptyBasketErrorModalClose}
+				handleCancel={handleEmptyBasketErrorModalClose}
+				header={'Cannot Save Basket'}
+				message="Basket should have atleast one position element to save."
+			/>
+		</>
+	)
+}
+
+export default Basket
